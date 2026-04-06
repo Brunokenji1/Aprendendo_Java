8c3f3c53d12dc3e32cd67bd0ee04930c436d093a
Bruno Okamoto <obrunokenji@gmail.com>
2026-04-05 22:31:49 -0300

    using Response API OMDb API

--- PROJECTS/screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ PROJECTS/screenmatch/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -5,14 +5,23 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = sc.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=ed7f6c6f";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://www.omdbapi.com/?t=avengers&apikey=ed7f6c6f"))
+                .uri(URI.create(endereco))
                 .build();
         HttpResponse<String> response = client
                 .send(request, HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
     }
 }
