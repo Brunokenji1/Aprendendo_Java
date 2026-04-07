79b7071b2e350fa0bed2ce93b16eae3bc65feadb
Bruno Okamoto <obrunokenji@gmail.com>
2026-04-06 23:28:11 -0300

    leaning GSON

--- PROJECTS/screenmatch/src/br/com/alura/screenmatch/modelos/Titulo.java
+++ PROJECTS/screenmatch/src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -1,10 +1,13 @@
 package br.com.alura.screenmatch.modelos;
 
 import br.com.alura.screenmatch.calculos.Classificavel;
+import com.google.gson.annotations.SerializedName;
 
 public class Titulo implements Comparable<Titulo> {
+    @SerializedName("Title")
     private String nome;
     private int duracao;
+    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluidoNoPlano;
     private double somaAvaliacao;
@@ -13,6 +16,12 @@ public class Titulo implements Comparable<Titulo> {
     public Titulo(String nome, int anoDeLancamento) {
         this.nome = nome;
         this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public Titulo(TituloOmdb meuTituloOmbdb) {
+        this.nome = meuTituloOmbdb.title();
+        this.anoDeLancamento = Integer.valueOf(meuTituloOmbdb.year());
+        this.duracao = Integer.valueOf(meuTituloOmbdb.runtime().substring(0,2));
     }
 
 //ALT + INS -> atalho fazer setters e getters
@@ -75,5 +84,12 @@ public class Titulo implements Comparable<Titulo> {
     @Override
     public int compareTo(Titulo outroTitulo) {
         return this.getNome().compareTo(outroTitulo.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "Nome= '" + nome + "\'," +
+                "anoDeLancamento=" + anoDeLancamento+
+                ", duração em minutos " + duracao;
     }
 }
